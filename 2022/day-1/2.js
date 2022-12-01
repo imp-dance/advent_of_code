413c1727737eafba9dbbd0c4724cf9d2b55411bc
Håkon Underbakke <haakon@underbakke.net>
2022-12-01 09:41:36 +0100

    chore: Day 1 solution

---
+++ 2022/day-1/2.js
@@ -0,0 +1,21 @@
+const content = document.querySelector("pre");
+
+const summed = content
+  .split("\n\n")
+  .map((chunk) => {
+    const sum = chunk.split("\n");
+    return sum
+      .map((i) => parseInt(i))
+      .reduce((acc, item) => {
+        acc += item;
+        return acc;
+      }, 0);
+    return sum;
+  })
+  .filter((n) => !Number.isNaN(n));
+
+summed.sort((a, b) => a - b);
+summed.reverse();
+
+// 203905
+return summed[0] + summed[1] + summed[2];

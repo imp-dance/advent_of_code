83181fd33d80e2ec73f100df0a495d0de0526880
Håkon <haakonu97@gmail.com>
2022-12-03 15:43:20 +0100

    refactor: Cleaned up code a bit

--- 2022/day-3.js
+++ 2022/day-3.js
@@ -1,5 +1,6 @@
 const { data } = require("./data/day-3");
 
+// Reduce data to a list of tuples, one tuple for each rucksack.
 const rucksacks = data.split("\n").map((line) => {
   const lineLength = line.length;
   const room1 = line.substring(0, lineLength / 2);
@@ -14,27 +15,25 @@ const alphabet =
 
 // Part 1:
 const part1 = () => {
-  const sums = [];
-  rucksacks.forEach((sacks) => {
-    let sharedLetters = [];
-    sacks[0].split("").forEach((item) => {
-      if (sharedLetters.includes(item)) {
-        // Ignore letter that's already been confirmed
-        return;
+  const sums = rucksacks.reduce((acc, sacks) => {
+    // Find the common letter in each sack.
+    const sharedLetter = alphabet.reduce((letterAcc, letter) => {
+      if (sacks.every((sack) => sack.includes(letter))) {
+        letterAcc = letter;
       }
-      if (sacks[1].includes(item)) {
-        sharedLetters.push(item); // letter is shared between sacks
-      }
-    });
-    sums.push(alphabet.indexOf(sharedLetters[0]) + 1);
-  });
-  const sum = sums.reduce((a, i) => a + i, 0);
+      return letterAcc;
+    }, "");
+    // Translate the letter to it's respective priority.
+    acc.push(alphabet.indexOf(sharedLetter) + 1);
+    return acc;
+  }, []);
 
-  return sum;
+  return sums.reduce((a, i) => a + i, 0);
 };
 
 // Part 2:
 const part2 = () => {
+  // Reduce the rucksacks to groups of three sacks.
   const groups = [];
   let currentGroupIndex = 0;
   rucksacks.forEach((sacks) => {
@@ -47,25 +46,34 @@ const part2 = () => {
     }
   });
 
-  const commonLetters = groups.reduce((acc, group) => {
-    let commonItem;
-    alphabet.forEach((letter) => {
-      if (group.every((item) => item.includes(letter))) {
-        commonItem = letter;
+  // Summize common letters for each group.
+  const commonLetters = groups.reduce(
+    (commonLetterAcc, group) => {
+      const sharedLetter = alphabet.reduce(
+        (sharedLetterAcc, letter) => {
+          if (group.every((item) => item.includes(letter))) {
+            sharedLetterAcc = letter;
+          }
+          return sharedLetterAcc;
+        },
+        ""
+      );
+      if (sharedLetter) {
+        commonLetterAcc.push(sharedLetter);
       }
-    });
-    if (commonItem) {
-      acc.push(commonItem);
-    }
-    return acc;
-  }, []);
+      return commonLetterAcc;
+    },
+    []
+  );
 
-  const sum = commonLetters.reduce((acc, letter) => {
-    acc += alphabet.indexOf(letter) + 1;
-    return acc;
+  // Summize the common letters.
+  const sum = commonLetters.reduce((sum, letter) => {
+    sum += alphabet.indexOf(letter) + 1;
+    return sum;
   }, 0);
 
   return sum;
 };
 
-part2();
+console.log(part1());
+console.log(part2());

d8a5a62d652dab45dc76011c54162799dee0868e
Håkon <haakonu97@gmail.com>
2022-12-09 21:44:12 +0100

    solution: day 9 part 2

--- 2022/data/day-9.js
+++ 2022/data/day-9.js
@@ -1,3 +1,12 @@
+const testData = `R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20`;
+
 const data = `L 1
 D 2
 L 2
--- 2022/day-9.js
+++ 2022/day-9.js
@@ -13,6 +13,17 @@ const tailPosition = {
 
 const state = {
   tailTouched: [{ ...tailPosition }],
+  tails: {
+    0: { ...tailPosition },
+    1: { ...tailPosition },
+    2: { ...tailPosition },
+    3: { ...tailPosition },
+    4: { ...tailPosition },
+    5: { ...tailPosition },
+    6: { ...tailPosition },
+    7: { ...tailPosition },
+    8: { ...tailPosition },
+  },
 };
 
 const parseLine = (acc, command) => {
@@ -38,41 +49,41 @@ const touchTail = (position) => {
   }
 };
 
-const followHead = () => {
+const calculateBestMove = (head, tail) => {
   const topLeftCorner = {
-    horizontal: tailPosition.horizontal - 1,
-    vertical: tailPosition.vertical - 1,
+    horizontal: tail.horizontal - 1,
+    vertical: tail.vertical - 1,
   };
   const topRightCorner = {
-    horizontal: tailPosition.horizontal + 1,
-    vertical: tailPosition.vertical - 1,
+    horizontal: tail.horizontal + 1,
+    vertical: tail.vertical - 1,
   };
   const bottomLeftCorner = {
-    horizontal: tailPosition.horizontal - 1,
-    vertical: tailPosition.vertical + 1,
+    horizontal: tail.horizontal - 1,
+    vertical: tail.vertical + 1,
   };
   const bottomRightCorner = {
-    horizontal: tailPosition.horizontal + 1,
-    vertical: tailPosition.vertical + 1,
+    horizontal: tail.horizontal + 1,
+    vertical: tail.vertical + 1,
   };
   const left = {
-    horizontal: tailPosition.horizontal - 1,
-    vertical: tailPosition.vertical,
+    horizontal: tail.horizontal - 1,
+    vertical: tail.vertical,
   };
   const right = {
-    horizontal: tailPosition.horizontal + 1,
-    vertical: tailPosition.vertical,
+    horizontal: tail.horizontal + 1,
+    vertical: tail.vertical,
   };
   const top = {
-    horizontal: tailPosition.horizontal,
-    vertical: tailPosition.vertical - 1,
+    horizontal: tail.horizontal,
+    vertical: tail.vertical - 1,
   };
   const bottom = {
-    horizontal: tailPosition.horizontal,
-    vertical: tailPosition.vertical + 1,
+    horizontal: tail.horizontal,
+    vertical: tail.vertical + 1,
   };
   const validMoves = [
-    tailPosition, // try current position first.
+    tail, // try current position first.
     topLeftCorner,
     top,
     topRightCorner,
@@ -83,61 +94,112 @@ const followHead = () => {
     left,
   ].filter(
     (option) =>
-      [-1, 0, 1].includes(
-        option.horizontal - headPosition.horizontal
-      ) &&
-      [-1, 0, 1].includes(
-        option.vertical - headPosition.vertical
-      )
+      [-1, 0, 1].includes(option.horizontal - head.horizontal) &&
+      [-1, 0, 1].includes(option.vertical - head.vertical)
   );
   if (
     validMoves.find(
       ({ horizontal, vertical }) =>
-        horizontal === tailPosition.horizontal &&
-        vertical === tailPosition.vertical
+        horizontal === tail.horizontal &&
+        vertical === tail.vertical
     )
   ) {
     // Staying put is a valid option, just do that.
-    return;
+    return { ...tail };
   }
 
   const distances = validMoves.map(
     (option) =>
-      Math.abs(option.horizontal - headPosition.horizontal) +
-      Math.abs(option.vertical - headPosition.vertical)
+      Math.abs(option.horizontal - head.horizontal) +
+      Math.abs(option.vertical - head.vertical)
   );
   const minDistance = Math.min(...distances);
   const optimalIndex = distances.findIndex(
     (distance) => distance === minDistance
   );
   const optimalNextStep = validMoves[optimalIndex];
-  tailPosition.horizontal = optimalNextStep.horizontal;
-  tailPosition.vertical = optimalNextStep.vertical;
-  touchTail(tailPosition);
-};
-
-const move = (command) => {
-  switch (command) {
-    case "U":
-      headPosition.vertical--;
-      break;
-    case "D":
-      headPosition.vertical++;
-      break;
-    case "L":
-      headPosition.horizontal--;
-      break;
-    case "R":
-      headPosition.horizontal++;
-      break;
-  }
-  followHead();
+  return optimalNextStep;
 };
 
 const part1 = () => {
   state.tailTouched = [{ horizontal: 0, vertical: 0 }];
+  const followHead = () => {
+    const bestMove = calculateBestMove(
+      headPosition,
+      tailPosition
+    );
+    tailPosition.horizontal = bestMove.horizontal;
+    tailPosition.vertical = bestMove.vertical;
+    touchTail(tailPosition);
+  };
+  const move = (command) => {
+    switch (command) {
+      case "U":
+        headPosition.vertical--;
+        break;
+      case "D":
+        headPosition.vertical++;
+        break;
+      case "L":
+        headPosition.horizontal--;
+        break;
+      case "R":
+        headPosition.horizontal++;
+        break;
+    }
+    followHead();
+  };
   commands.forEach((dir) => move(dir));
   return state.tailTouched.length;
 };
 
+const part2 = () => {
+  headPosition.horizontal = 0;
+  headPosition.vertical = 0;
+  state.tailTouched = [{ horizontal: 0, vertical: 0 }];
+
+  const followHead = (i) => {
+    // Move the tail-head relative to the head.
+    const bestMove = calculateBestMove(
+      headPosition,
+      state.tails[0]
+    );
+    state.tails[0].horizontal = bestMove.horizontal;
+    state.tails[0].vertical = bestMove.vertical;
+
+    // Move the rest of the tails relative to their previous index
+    for (let i = 1; i < 9; i++) {
+      const bestMove = calculateBestMove(
+        state.tails[i - 1],
+        state.tails[i]
+      );
+      state.tails[i].horizontal = bestMove.horizontal;
+      state.tails[i].vertical = bestMove.vertical;
+    }
+    touchTail(state.tails[8]);
+  };
+
+  const move = (command, i) => {
+    switch (command) {
+      case "U":
+        headPosition.vertical--;
+        break;
+      case "D":
+        headPosition.vertical++;
+        break;
+      case "L":
+        headPosition.horizontal--;
+        break;
+      case "R":
+        headPosition.horizontal++;
+        break;
+    }
+    followHead(i);
+  };
+  commands.forEach(move);
+
+  return state.tailTouched.length;
+};
+
 console.log(part1());
+console.log(part2());

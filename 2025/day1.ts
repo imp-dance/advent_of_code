5cd8aed95b6d3ba2eae752308a5cb8966b932462
Håkon <haakonu97@gmail.com>
2025-12-01 17:09:06 +0100

    chore: cleanup

--- 2025/day1.ts
+++ 2025/day1.ts
@@ -1,63 +1,65 @@
 import fs from "fs";
 
-const wrap = (value: number, mod: number) => {
-  return ((value % mod) + mod) % mod;
+const input = fs.readFileSync("./day1.input.txt", "utf8");
+
+const parseLine = (line: string) =>
+  [
+    line[0] as "L" | "R",
+    parseInt(line.substring(1).trim()),
+  ] as const;
+
+const lines = input.split("\n").map(parseLine);
+
+const state = {
+  num: 50,
 };
 
-const input = fs.readFileSync("./day1.input.txt", "utf8");
-const lines = input
-  .split("\n")
-  .map(
-    (line) =>
-      [
-        line[0] as "L" | "R",
-        parseInt(line.substring(1).trim()),
-      ] as const
-  );
-
-lines.forEach(([dir, degree], index) => {
-  if (Number.isNaN(degree)) console.log("At ", index);
-});
-
-let num = 50;
-const turn = (direction: "L" | "R", degrees: number) => {
-  if (direction === "R") {
-    num = wrap(num + degrees, 100);
-  } else {
-    num = wrap(num - degrees, 100);
-  }
+const resetState = () => {
+  state.num = 50;
 };
 
 const part1 = () => {
-  num = 50;
+  resetState();
   let timesHitOne = 0;
   lines.forEach(([direction, degrees]) => {
     turn(direction, degrees);
-    if (num === 0) timesHitOne++;
+    if (state.num === 0) timesHitOne++;
   });
   return timesHitOne;
 };
 
 const part2 = () => {
+  resetState();
   let timesPassedOne = 0;
-  num = 50;
   lines.forEach(([direction, degrees]) => {
     let passedAtleast = Math.floor(degrees / 100);
     const rest = degrees % 100;
-    if (direction === "R" && rest + num > 100) {
+    if (direction === "R" && rest + state.num > 100) {
       passedAtleast += 1;
     } else if (
       direction === "L" &&
-      num - rest < 0 &&
-      num !== 0
+      state.num - rest < 0 &&
+      state.num !== 0
     ) {
       passedAtleast += 1;
     }
     timesPassedOne += passedAtleast;
     turn(direction, degrees);
-    if (num === 0) timesPassedOne += 1;
+    if (state.num === 0) timesPassedOne += 1;
   });
   return timesPassedOne;
 };
 
-console.log(part2());
+console.log({ part1: part1(), part2: part2() });
+
+function turn(direction: "L" | "R", degrees: number) {
+  if (direction === "R") {
+    state.num = wrap(state.num + degrees, 100);
+  } else {
+    state.num = wrap(state.num - degrees, 100);
+  }
+}
+
+function wrap(value: number, mod: number) {
+  return ((value % mod) + mod) % mod;
+}
